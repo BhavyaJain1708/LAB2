6fc5760fd016543f061e0f0eefb83a330d23c4d4
BhavyaJain1708 <143415793+BhavyaJain1708@users.noreply.github.com>
2023-09-14 00:12:55 +0530

    Add files via upload

---
+++ Weekdays.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main ()
+{
+	int n;
+	printf("The day number is");
+	scanf("%d",&n);
+	if (n==1)
+	printf("Monday");
+	else if (n==2)
+	printf("Tuesday");
+	else if (n==3)
+	printf("Wednesday");
+	else if (n==4)
+	printf("Thursday");
+	else if (n==5)
+	printf("Friday");
+	else if (n==6)
+	printf("Saturday");
+	else if (n==7) 
+	printf("Sunday");
+	else 
+	printf("Please enter number between 1-7");
+	return 0;
+}
