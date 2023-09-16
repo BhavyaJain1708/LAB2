5cf38be6b3906bcf0512b39ae10bd88f2ca1ff91
BhavyaJain1708 <143415793+BhavyaJain1708@users.noreply.github.com>
2023-09-16 16:07:46 +0530

    Input Character.cpp

--- Input Character.cpp
+++ Input Character.cpp
@@ -3,11 +3,11 @@
 int main ()
 {
 	char I;
-	scanf("%char",&I);
+	scanf("%c",&I);
 	if ((I>=65 && I<=90)||(I>=97 && I<=122))
-	printf("The input character is Alphabet");
+	printf("The input character is an Alphabet");
 	else if (I>=48 && I<=57)
-	printf("The input character is Digit");
+	printf("The input character is a Digit");
 	else
 	printf("The input character is a Special Character");
 	return 0;
